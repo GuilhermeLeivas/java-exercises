d88441864ca13457d56b378a829f681bbee71f26
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-02-05 16:06:26 -0300

    Exercise 2

---
+++ src/br/com/leivas/exercise2/BiCycle.java
@@ -0,0 +1,8 @@
+package br.com.leivas.exercise2;
+
+public abstract class BiCycle {
+
+    protected String defineMe() {
+        return "A vehicle with pedals.";
+    }
+}
---
+++ src/br/com/leivas/exercise2/Solution.java
@@ -0,0 +1,8 @@
+package br.com.leivas.exercise2;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        final MotorCycle motorCycle = new MotorCycle();
+    }
+}
