b68f3c0eba3910f705e041f0ba9358fba9c9dce2
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-02-05 19:08:23 -0300

    Exercise 9

---
+++ src/br/com/leivas/exercise9/Cake.java
@@ -0,0 +1,13 @@
+package br.com.leivas.exercise9;
+
+public class Cake implements Food {
+    @Override
+    public String getType() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public void makeFood() {
+        System.out.printf("Making a delicious %s\n", this.getType());
+    }
+}
---
+++ src/br/com/leivas/exercise9/FoodFactory.java
@@ -0,0 +1,23 @@
+package br.com.leivas.exercise9;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FoodFactory implements Factory<String, Food> {
+
+    @Override
+    public Food produce(String input) {
+        try {
+            String packageName = "br.com.leivas.exercise9.";
+            String fullQualifiedName = packageName + input;
+            final Class<?> classRef = Class.forName(fullQualifiedName);
+            final Constructor<?> constructor = classRef.getConstructor();
+            return (Food) constructor.newInstance();
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            Logger.getLogger(this.getClass().getSimpleName()).log(Level.SEVERE, "Failed to produce the request input: " + input);
+        }
+        return null;
+    }
+}
