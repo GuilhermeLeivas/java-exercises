7a2d3fbe229158bddfa43b6fc975e1e46049da71
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-02-05 17:41:41 -0300

    Exercise 6

---
+++ src/br/com/leivas/exercise6/ArraySumerUtil.java
@@ -0,0 +1,44 @@
+package br.com.leivas.exercise6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class ArraySumerUtil {
+
+    private final Scanner scanner;
+    private int elementsLength = 0;
+    private int[] array;
+
+    public ArraySumerUtil() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void readElementsTotal() {
+        System.out.println("Type how many elements it will be in the array\n");
+        this.elementsLength = this.scanner.nextInt();
+        this.array = new int[this.elementsLength];
+    }
+
+    public void readEachElement() {
+        if (this.elementsLength != 0) {
+            for (int i = 0; i < this.elementsLength; i++) {
+                System.out.println("Type element " + (i + 1));
+                this.array[i] = this.scanner.nextInt();
+            }
+        }
+    }
+
+    public void printAllElements() {
+        System.out.println("All elements: " + Arrays.toString(this.array));
+    }
+
+    public void printSumOfAllElements() {
+        if (this.array == null || this.array.length == 0) {
+            System.out.println("Empty array, so the sum is 0");
+            return;
+        }
+        int sum = IntStream.of(this.array).sum();
+        System.out.println("The sum of all elements in the array is " + sum);
+    }
+}
---
+++ src/br/com/leivas/exercise6/Solution6.java
@@ -0,0 +1,11 @@
+package br.com.leivas.exercise6;
+
+public class Solution6 {
+    public static void main(String[] args) {
+        ArraySumerUtil arraySumerUtil = new ArraySumerUtil();
+        arraySumerUtil.readElementsTotal();
+        arraySumerUtil.readEachElement();
+        arraySumerUtil.printAllElements();
+        arraySumerUtil.printSumOfAllElements();
+    }
+}
