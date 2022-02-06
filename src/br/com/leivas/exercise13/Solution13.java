991cbf289d1ecd7944a80dbfe11bf26976abb70b
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-02-06 00:06:27 -0300

    Exercise 13

--- src/br/com/leivas/exercise13/Solution13.java
+++ src/br/com/leivas/exercise13/Solution13.java
@@ -3,11 +3,10 @@ package br.com.leivas.exercise13;
 public class Solution13 {
     public static void main(String[] args) {
 
-        LinkedListImpl list = new LinkedListImpl();
+        ILinkedList<LinkedListImpl.Node, Integer> list = new LinkedListImpl();
         LinkedListImpl.Node head = list.insertAtPosition(null, 3, 0);
         LinkedListImpl.Node head1 = list.insertAtPosition(head, 4, 0);
         final LinkedListImpl.Node head2 = list.insert(head1, 55);
         LinkedListImpl.Node head3 = list.insertAtPosition(head1, 5, 2);
-        list.printList(list);
     }
 }
