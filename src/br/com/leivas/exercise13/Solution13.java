c258918437d334e694782a7e9b43048aa4d5cbaf
Guilherme Leivas <guilhermeleivas00@hotmail.com>
2022-02-07 02:48:39 -0300

    Correction Solution8; Comment correction on Solution13;

--- src/br/com/leivas/exercise13/Solution13.java
+++ src/br/com/leivas/exercise13/Solution13.java
@@ -9,6 +9,6 @@ public class Solution13 {
         final LinkedListImpl.Node head2 = list.insert(head1, 55);
         final LinkedListImpl.Node head3 = list.insertAtPosition(head2, 5, 2);
         final LinkedListImpl.Node head4 = list.insertAtPosition(head3, 85, 0);
-        // this was in exercise 13: "Do NOT print anything to stdout/console.". So, i won't print anything on the console.
+        // In exercise 13 it says: "Do NOT print anything to stdout/console.". So, i won't print anything on the console.
     }
 }
--- src/br/com/leivas/exercise8/Solution8.java
+++ src/br/com/leivas/exercise8/Solution8.java
@@ -5,7 +5,8 @@ public class Solution8 {
     public static void main(String[] args) {
         final MySingleton instance1 = MySingleton.getInstance();
         final MySingleton instance2 = MySingleton.getInstance();
-        System.out.println("Are these same instance ? " + instance1.equals(instance2));
+        boolean areTheseSameInstance = (instance1 == instance2);
+        System.out.println("Are these same instance ? " + (areTheseSameInstance ? "YES" : "NOOO"));
     }
 }
 
